b735836637ce5d04a504ff9fb5c12e856267bf90
wul <wulan.mantiri@gmail.com>
2021-01-26 22:48:53 +0800

    Reduce text and change dark theme colors

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -5,9 +5,9 @@ export const EXPERIENCES_LIST = [
     place: 'CS UI Student Council (Depok, Indonesia)',
     descriptions: [
       `I collaborated with a team of 10 in ##designing and developing the frontend requirements##
-      with ##React.js## for the following two web applications:`,
-      `(1) Requirements Gatherer app for organizations to handle project requests from clients, and`,
-      `(2) Blood donation app handling donor registry and management for Palang Merah Indonesia.`,
+      with ##React.js## for (1) Requirements Gatherer app for organizations to handle project
+      requests from clients, and (2) Blood donation app handling donor registry and management
+      for Palang Merah Indonesia.`,
     ],
   },
   {
@@ -15,13 +15,9 @@ export const EXPERIENCES_LIST = [
     time: 'Sep 2019 - Jan 2021',
     place: 'University of Indonesia (Depok, Indonesia)',
     descriptions: [
-      `##Web Design and Programming##: I mentored 15 students on weekly tasks and final
-      Django-based projects with TDD.`,
-      `##Java Fundamentals##: I facilitated weekly labs, created lab exercises, and
-      supervised 15 students in learning OOP.`,
-      `##Python Fundamentals##: I tutored 7 students to comprehend programming basics and paradigm.`,
-      `##Calculus##: I graded assignments for 25 students and conducted weekly tutorial
-      sessions for 15-80 students.`,
+      `I faciliated and created weekly lab exercises/tutorials and supervised 15 students
+      on ##Calculus##, ##Programming Foundations I and II## with Python and Java, and
+      ##Web Design and Programming## with Django.`,
     ],
   },
   {
@@ -29,15 +25,9 @@ export const EXPERIENCES_LIST = [
     time: 'Jul 2020 - Oct 2020',
     place: '9CV9 (Ho Chi Minh, Vietnam)',
     descriptions: [
-      `I ##collaborated in maintaining 9CV9 career platform##, creating and improving features,
-      revising admin management, resolving production data inconsistency, and integrating
-      with online platform partners using ##agile development## with ##Django REST##.`,
-      `Furthermore, I ##reduced user input time and manual labor## by building a new Python-based
-      resume parser to extract information from PDF or Word documents and web scraper
-      that automate filling form and data retrieval processes.`,
-      `I also ##improved user traffic## by handling SEO from the backend side, creating personality
-      assessment endpoints, and developing a chat with read and online feature, which doubled
-      the number of registered users to 2000+ users.`,
+      `I collaborated in maintaining 9CV9 career platform, built a new ##Python-based##
+      resume parser and web scraper, handled SEO from the backend side, and developed
+      personality assessment and chat feature using ##agile development## with ##Django REST##.`,
     ],
   },
   {
@@ -45,9 +35,8 @@ export const EXPERIENCES_LIST = [
     time: 'Feb 2020 - present',
     place: 'High School Model United Nations (Jakarta, Indonesia)',
     descriptions: [
-      `I ##lead the academics team## in managing the process of creating council curriculum
-      and study guides, setting timeline, recruiting board of directors,
-      and allocating delegates to their respective roles and councils.`,
+      `I lead the academics team in managing the councils' curriculum and study guides
+      creation, timeline, chair recruitment, and delegate registration processes.`,
     ],
   },
   {
@@ -55,7 +44,7 @@ export const EXPERIENCES_LIST = [
     time: 'Feb 2019 - Jan 2020',
     place: 'CS UI Student Council (Depok, Indonesia)',
     descriptions: [
-      `I ##coordinated the selection of Most Outstanding Student## to represent Faculty of
+      `I coordinated the selection of Most Outstanding Student to represent Faculty of
       Computer Science, earning 2nd place among UI's science faculties.`,
     ],
   },
@@ -64,7 +53,7 @@ export const EXPERIENCES_LIST = [
     time: 'Jan 2019 - Jan 2020',
     place: 'UI Model United Nations Club (Depok, Indonesia)',
     descriptions: [
-      `I ##facilitated recruitments and trainings for delegation## that will represent
+      `I facilitated recruitments and trainings for delegation that will represent
       UI MUN Club in national MUN competitions.`,
     ],
   },
@@ -73,9 +62,8 @@ export const EXPERIENCES_LIST = [
     time: 'Feb 2019 - Sep 2019',
     place: 'COMPFEST 11 (Depok, Indonesia)',
     descriptions: [
-      `I ##streamlined the searching of judges, mentors, and case study## for the final round,
-      and facilitated in the preliminary screening of 100+ teams in the biggest IT event held by
-      students in Indonesia.`,
+      `I streamlined the searching of judges, mentors, and case study for the final round,
+      and facilitated in the preliminary screening of 100+ teams.`,
     ],
   },
 ]
--- src/theme/dark/index.ts
+++ src/theme/dark/index.ts
@@ -6,10 +6,10 @@ const colors = {
   sidebar: '#7F7F7F',
   primary: '#000000',
   secondary: '#F7F7F7',
-  evenSection: '#060606',
-  oddSection: '#0b0b0b',
+  evenSection: '#0a0a0a',
+  oddSection: '#060606',
   contrast: '#FFFFFF',
-  highlightBlue: '#2b303c',
+  highlightBlue: '#21416e',
   navbar: 'rgba(10, 10, 10, 0.7)',
 
   // constant
